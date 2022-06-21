e0cf447d0961b4e4a3feb11529fc96d3274ba330
PK <pradeepdhital124@gmail.com>
2022-06-21 11:13:21 +1000

    here we used fetch method to handle data accessed from web api

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const countriesContainer = document.querySelector('.countries');
 const renderCountry = function (data, className) {
   const language = Object.values(data.languages);
   const currency = Object.values(data.currencies);
-  const html = `  
+  const html = `
          <article class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
@@ -23,25 +23,39 @@ const renderCountry = function (data, className) {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
-const getCountriesData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    renderCountry(data);
-    const neighbour = Object.values(data.borders);
-    const request2 = new XMLHttpRequest();
-    request2.open(
-      'GET',
-      `https://restcountries.com/v3.1/alpha/${neighbour[0]}`
-    );
-    request2.send();
-    request2.addEventListener('load', function () {
-      const [data2] = JSON.parse(this.responseText);
-      renderCountry(data2, 'neighbourCountry');
-    });
-  });
-};
+// const getCountriesData = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     renderCountry(data);
+//     const neighbour = Object.values(data.borders);
+//     const request2 = new XMLHttpRequest();
+//     request2.open(
+//       'GET',
+//       `https://restcountries.com/v3.1/alpha/${neighbour[0]}`
+//     );
+//     request2.send();
+//     request2.addEventListener('load', function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       renderCountry(data2, 'neighbourCountry');
+//     });
+//   });
+// };
 
-getCountriesData('nepal');
+// getCountriesData('nepal');
+
+// Here we are doing AJAX call using modern ES6 feature call fetch API method.
+// It might be  better idea to use arrow function in the following case
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (datas) {
+      const [data] = datas;
+      renderCountry(data);
+    });
+};
+getCountryData('portugal');
